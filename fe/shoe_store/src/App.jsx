d4343865a8075e95a66043ec045fe0905b095cd5
phananhduc2003 <pananhduc2003@gmail.com>
2024-11-17 00:35:30 +0700

    setup project

--- fe/shoe_store/src/App.jsx
+++ fe/shoe_store/src/App.jsx
@@ -1,7 +1,12 @@
+import Button from "@mui/material/Button";
+
 function App() {
   return (
     <>
       <p>hello world</p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   );
 }
